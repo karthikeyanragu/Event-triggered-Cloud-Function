d98237c3f97f96bd43e220406067a0ac7aff4b62
Karthikeyan R <104515955+karthikeyanragu@users.noreply.github.com>
2023-06-25 20:45:03 +0530

    Add files via upload

---
+++ spark-demo.py
@@ -0,0 +1,42 @@
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType, DoubleType, BooleanType, TimestampType
+import sys
+
+bucket_name = sys.argv[1]
+file_name = sys.argv[2]
+
+spark = SparkSession.builder.appName('demo').getOrCreate()
+
+
+schema = StructType([StructField("VIN", StringType(), False),
+                     StructField("County", StringType(), True),
+                     StructField("City", StringType(), True),
+                     StructField("State", StringType(), True),
+                     StructField("Postal_Code", IntegerType(), True),
+                     StructField("Model_Year", IntegerType(), True),
+                     StructField("Make", StringType(), True),
+                     StructField("Model", StringType(), True),
+                     StructField("Electric_Vehicle_Type", StringType(), True),
+                     StructField("CAFV_Eligibility", StringType(), True),
+                     StructField("Electric_Range", IntegerType(), True),
+                     StructField("Base_MSRP", IntegerType(), True),
+                     StructField("Legislative_District", IntegerType(), True),
+                     StructField("DOL_Vehicle_ID", IntegerType(), True),
+                     StructField("Vehicle_Location", StringType(), True),
+                     StructField("Electric_Utility", StringType(), True),
+                     StructField("2020_Census_Tract", IntegerType(), True),
+                     StructField("insert_time", TimestampType(), True)])
+
+
+df  = spark.read.option("header" , True).schema(schema).csv(f'gs://{bucket_name}/{file_name}')
+
+
+df1= df.withColumn('insert_time',current_timestamp())
+
+df1.write.format('bigquery') \
+    .option('table', 'persuasive-pipe-384604.bigdata.ev_pop') \
+    .option('temporaryGcsBucket', 'my_bigdata_bucket007') \
+    .mode('overwrite') \
+    .save()
