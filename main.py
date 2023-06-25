67a9ca735077993d1237fe9bdaf99924063c9919
Karthikeyan R <104515955+karthikeyanragu@users.noreply.github.com>
2023-06-25 19:39:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
+import pandas as pd
+from pandas.io import gbq
 from google.cloud import bigquery
 
-def trigger_bqclientload(event, context):
-    
+
+def hello_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
     file_name = event['name']
     bucket_name = event['bucket']
-    table_id = f"persuasive-pipe-384604.cloudfunction.{event['name'].split('.')[0]}"
-    meta_table = 'persuasive-pipe-384604.cloudfunction.data_loading_metadata'
-    
-    
-    
-    client = bigquery.Client()
-    job_config = bigquery.LoadJobConfig(autodetect = True, create_disposition='CREATE_IF_NEEDED',
-                                        source_format = bigquery.SourceFormat.CSV, 
-                                        write_disposition='WRITE_APPEND')
+    table_name = file_name.split('.')[0]
 
-    
-    
-    uri = f"gs://{bucket_name}/{file_name}"
-    load_job = client.load_table_from_uri(uri , table_id,job_config=job_config )
-    load_job.result()
-     
-    
-    destination_table = client.get_table(table_id)
-    
-    meta_dict = [ {
+    metadata = []
+    metadata_dict = {
       'Event_ID' : context.event_id,
       'Event_Type' : context.event_type,
       'Bucket_Name' : event['bucket'],
       'File_name' : event['name'],
       'Created' : event['timeCreated'],
-      'Updated' : event['updated'],
-      'Rows_loaded' : load_job.output_rows
-    } ]
+      'Updated' : event['updated']
+                    }
 
-    insert_metadata = client.load_table_from_json(meta_dict,meta_table, num_retries = 2,job_config=job_config)
-    insert_metadata.result()
-    
-    print(f"Successfully loaded {load_job.output_rows} rows.")
-    
-    return f'check the results in the logs'
+    metadata.append(metadata_dict)
+
+    df_metadata = pd.DataFrame.from_records(metadata)
+    df_metadata.to_gbq('cloudfunction.data_loading_metadata',
+                           project_id = 'persuasive-pipe-384604',
+                           if_exists = 'append',
+                           location = 'us')
+
+    df_data = pd.read_csv('gs://' + event['bucket'] + '/' + event['name'])
+
+    df_data.to_gbq('cloudfunction.' + table_name,
+                           project_id = 'persuasive-pipe-384604',
+                           if_exists = 'append',
+                           location = 'us')
+
