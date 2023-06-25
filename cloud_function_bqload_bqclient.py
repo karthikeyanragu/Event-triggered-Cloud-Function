b0f13a6e22cb50245700041c368218c4943946c0
Karthikeyan R <104515955+karthikeyanragu@users.noreply.github.com>
2023-06-25 20:41:42 +0530

    Update and rename main.py to cloud_function_bqload_bqclient.py

--- cloud_function_bqload_bqclient.py
+++ cloud_function_bqload_bqclient.py
@@ -3,7 +3,7 @@ from pandas.io import gbq
 from google.cloud import bigquery
 
 
-def hello_gcs(event, context):
+def bq_client_load(event, context):
     """Triggered by a change to a Cloud Storage bucket.
     Args:
          event (dict): Event payload.
@@ -27,14 +27,14 @@ def hello_gcs(event, context):
 
     df_metadata = pd.DataFrame.from_records(metadata)
     df_metadata.to_gbq('cloudfunction.data_loading_metadata',
-                           project_id = 'persuasive-pipe-384604',
+                           project_id = 'xxxxxxx-yyyyy-384604',
                            if_exists = 'append',
                            location = 'us')
 
     df_data = pd.read_csv('gs://' + event['bucket'] + '/' + event['name'])
 
     df_data.to_gbq('cloudfunction.' + table_name,
-                           project_id = 'persuasive-pipe-384604',
+                           project_id = 'xxxxxxx-yyyyy-384604',
                            if_exists = 'append',
                            location = 'us')
 
